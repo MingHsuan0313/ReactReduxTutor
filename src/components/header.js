0c071b14b42b6c5f510dc9e2b2d5b26c0a454f70
TimHsieh <timhost1233@gmail.com>
2018-07-04 04:48:28 +0800

    fix header.js

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Header = () =>{
+    // this component unlike react.component class it can't has it's own class
+    return(
+        <div>
+            Hello I am from stateless component header
+        </div>
+    )
+}
