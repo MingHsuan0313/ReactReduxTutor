cd062107fb04bc4486e5b1db87bd872f953bbc5f
TimHsieh <timhost1233@gmail.com>
2018-07-04 04:11:06 +0800

    add stateless component Header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import Header from "./components/header"
 
 class Container extends React.Component{
+    constructor(props){
+        super(props)
+    }
     render(){
         return(
             <div>
-                Hello
+                <Header/>
+                
             </div>
         )
     }
