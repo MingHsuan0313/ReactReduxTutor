960b58d2f1a3ef7b0c2799b46fbf307f1b6e914e
TimHsieh <timhost1233@gmail.com>
2018-07-04 04:13:40 +0800

    add some props and inline js in {} react JSX

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import Header from "./components/header"
 class Container extends React.Component{
     constructor(props){
         super(props)
+        self.text = "I have this state in stateful class\n"
+        self.list = ["1","2","3","4","5"]
     }
     render(){
         return(
             <div>
                 <Header/>
-                
+                {self.text}
+                {"inline javascript can put here"}
+                {
+                    self.list.map((data) =>{
+                        return(data)
+                    })
+                }
             </div>
         )
     }
