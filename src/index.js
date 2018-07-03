857ec872f83e640a8d86743b511f511f0b0995d6
TimHsieh <timhost1233@gmail.com>
2018-07-04 04:35:27 +0800

    show the simplest logic in redux

--- src/index.js
+++ src/index.js
@@ -1,27 +1,61 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Header from "./components/header"
+import logger from "redux-logger"
+import {createStore, applyMiddleware} from "redux"
 
-class Container extends React.Component{
-    constructor(props){
-        super(props)
-        self.text = "I have this state in stateful class\n"
-        self.list = ["1","2","3","4","5"]
+// reducer is the function who will handle all dispatch function fired from store object
+// it's default tow argument is state and action
+// state is the global object it will save in the browser
+// action is the object get from store.dispatch
+const mathReducer = (state = 0, action) =>{
+    switch(action.type){
+        case "add":
+            state = state + action.payload 
+        break
+
+        case "sub":
+            state = state - action.payload 
+        break
+
+        case "div":
+            state = state / action.payload
+        break
+
+        case "mul":
+            state = state * action.payload
+        break
     }
-    render(){
-        return(
-            <div>
-                <Header/>
-                {self.text}
-                {"inline javascript can put here"}
-                {
-                    self.list.map((data) =>{
-                        return(data)
-                    })
-                }
-            </div>
-        )
-    }
+    return state
 }
 
-ReactDOM.render(<Container/>,document.getElementById("react-container"))
+//second argument means the default state 0
+//third argument means the middleware that will show the log in the console.log
+const store = createStore(mathReducer,0,applyMiddleware(logger))
+
+store.subscribe( () =>{
+    console.log("update")
+})
+// below is something like this
+// 0 (default state defined in createStore)
+// (((0 + 2) * 5) - 7) / 2 = 1.5
+// you can see the result in F12 developer console
+store.dispatch({
+    type: "add",
+    payload: 2
+})
+
+store.dispatch({
+    type: "mul",
+    payload: 5
+})
+
+store.dispatch({
+    type: "sub",
+    payload: 7
+})
+
+store.dispatch({
+    type: "div",
+    payload: 2
+})
+
+
+
